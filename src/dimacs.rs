efa562f4130c13ce82928d9bdbd9f0a5c45c2de9
togatoga <togasakitogatoga@gmail.com>
2020-02-29 19:02:46 +0900

    Add support for parsing vars and clauses

--- src/dimacs.rs
+++ src/dimacs.rs
@@ -40,6 +40,31 @@ fn parse_int(input: &[char], idx: &mut usize) -> Result<i32, failure::Error> {
     Ok(value)
 }
 
+fn parse_vars_and_clauses(input: &[char], idx: &mut usize) -> Result<(i32, i32), failure::Error> {
+    //p cnf <variable num> <clause num>
+    //e.g. p cnf 90 300
+    skip_whitespace(input, idx);
+    if *idx >= input.len() {
+        return Err(format_err!("Parse failure {:?}", input));
+    }
+    assert_eq!(input[*idx], 'p');
+    *idx += 1;
+    skip_whitespace(input, idx);
+    if *idx + 2 < input.len() {
+        let cnf: String = input[*idx..*idx + 3].iter().collect();
+        if &cnf != "cnf" {
+            return Err(format_err!("cnf is not found {:?}", input));
+        } else {
+            *idx += 3;
+        }
+    }
+    skip_whitespace(input, idx);
+    let parsed_vars = parse_int(input, idx)?;
+    let parsed_clauses = parse_int(input, idx)?;
+
+    Ok((parsed_vars, parsed_clauses))
+}
+
 fn skip_whitespace(input: &[char], idx: &mut usize) {
     while *idx < input.len() {
         let c = input[*idx];
@@ -77,18 +102,16 @@ pub fn parse_dimacs_file(
             if c == 'p' {
                 //p cnf <variable num> <clause num>
                 //e.g. p cnf 90 300
-
-                //skip it until char is digit
-                while idx < line.len() {
-                    if line[idx].is_digit(10) {
+                match parse_vars_and_clauses(&line, &mut idx) {
+                    Ok((vars, clauses)) => {
+                        parsed_vars = Some(vars);
+                        parsed_clauses = Some(clauses);
+                    }
+                    _ => {
+                        //skip line
                         break;
-                    } else {
-                        idx += 1;
                     }
                 }
-                parsed_vars = Some(parse_int(&line, &mut idx)?);
-                parsed_clauses = Some(parse_int(&line, &mut idx)?);
-                break;
             } else {
                 //NOTE
                 //ReadClause
@@ -114,5 +137,64 @@ mod test {
             let parsed_num = parse_int(&num_chars, &mut idx).unwrap();
             assert_eq!(parsed_num, num);
         }
+        let num_chars: Vec<char> = "0000001".chars().collect();
+        let mut idx = 0;
+        assert_eq!(parse_int(&num_chars, &mut idx).unwrap(), 1);
+
+        let num_chars: Vec<char> = "+810".chars().collect();
+        let mut idx = 0;
+        let parsed_num = parse_int(&num_chars, &mut idx).unwrap();
+        assert_eq!(parsed_num, 810);
+
+        //extra whitespace
+        let num_chars: Vec<char> = " -10 ".chars().collect();
+        let mut idx = 0;
+        let parsed_num = parse_int(&num_chars, &mut idx).unwrap();
+        assert_eq!(parsed_num, -10);
+
+        //invalid cases
+        let num_chars: Vec<char> = "123-10".chars().collect();
+        let mut idx = 0;
+        assert!(parse_int(&num_chars, &mut idx).is_err());
+        let num_chars: Vec<char> = "----12310".chars().collect();
+        let mut idx = 0;
+        assert!(parse_int(&num_chars, &mut idx).is_err());
     }
+
+    #[test]
+    fn test_parse_vars_and_clauses() {
+        let line: Vec<char> = "p cnf 1000 100000".to_string().chars().collect();
+        let mut idx = 0;
+        assert_eq!(
+            parse_vars_and_clauses(&line, &mut idx).unwrap(),
+            (1000, 100000)
+        );
+        //extra whitespace
+        let line: Vec<char> = "      p             cnf     1000     100000   "
+            .to_string()
+            .chars()
+            .collect();
+        let mut idx = 0;
+        assert_eq!(
+            parse_vars_and_clauses(&line, &mut idx).unwrap(),
+            (1000, 100000)
+        );
+        //invalid cases
+        let line: Vec<char> = "      p             cnf     1000   "
+            .to_string()
+            .chars()
+            .collect();
+        let mut idx = 0;
+        assert!(parse_vars_and_clauses(&line, &mut idx).is_err());
+        let line: Vec<char> = "      p                  1000   10000"
+            .to_string()
+            .chars()
+            .collect();
+        let mut idx = 0;
+        assert!(parse_vars_and_clauses(&line, &mut idx).is_err());
+        let line: Vec<char> = "p    cnf".to_string().chars().collect();
+        let mut idx = 0;
+        assert!(parse_vars_and_clauses(&line, &mut idx).is_err());
+    }
+
 }
